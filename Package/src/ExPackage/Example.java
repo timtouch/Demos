a1034fb7a11f80de0a56a2f7826070be2d5f9401
timtouch <chaosbounder@gmail.com>
2018-07-06 08:52:35 -0500

    Game/Package

---
+++ Package/src/ExPackage/Example.java
@@ -0,0 +1,16 @@
+package ExPackage;
+
+/**
+  @params
+*/
+public class Example {
+  /*
+    Multi-line commenting is also useful
+  */
+  // String[] args - contains the command line arguments
+  public static void main(String[] args){
+    System.out.println("Number of args: " + args.length);
+
+    System.out.println("third arg: " + args[2]);
+  }
+}
